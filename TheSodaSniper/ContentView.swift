1307bc5707aedac07763cf1ed449906e8201dde1
Sam <salmanar644@gmail.com>
2026-06-23 23:15:09 +0700

    Initial Commit

---
+++ TheSodaSniper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheSodaSniper
+//
+//  Created by Salman on 23/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheSodaSniper/TheSodaSniperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSodaSniperApp.swift
+//  TheSodaSniper
+//
+//  Created by Salman on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TheSodaSniperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
